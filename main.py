e5126637afe3cff4f726595cb58370a8618a4ed8
bakul takthar <bakul takthar>
2023-02-01 20:59:56 +0530

    modification of attributes

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 print("hello world")
 class Calculator:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
-        
+        self.age = age
         
     def add(self, num1, num2):
         return num1 + num2, self.name
     
-    def dog(self):
-        return "bow bow!", self.name
+    def get_name(self):
+        return self.name
     
-c = Calculator("guddu")
+    def new_name(self, name):
+        self.name = name
+        
+    def get_age(self):
+        return self.age
+    
+    def set_age(self, age):
+        self.age = age
+        
+        
+c = Calculator("guddu", 67)
 
-print(c.add(3,5))
-print(c.name)
+print(c.get_age())
+print(c.get_name())
 
-c2 = Calculator("duggu")
-print(c2.dog())
-print(c2.name)
+print(".... \n\n\n\n\n .....")
+
+c.set_age(69)
+c.new_name("yo mama")
+print(c.get_age())
+print(c.get_name())
