3057ce7020651d8be15b1be49af3c5d5a1c7fc95
bakul takthar <bakul takthar>
2023-02-01 20:25:33 +0530

    classes and attributes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("hello world")
+class Calculator:
+    def add(self, num1, num2):
+        return num1 + num2
+    
+    def dog(self):
+        return "bow bow!"
+    
+c = Calculator()
+
+print(c.add(3,5))
