bb8bc1e603c601e844176235ce8eb75c9e0d2784
bakul takthar <bakul takthar>
2023-02-01 20:33:56 +0530

    added self arguments for the entire class

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 print("hello world")
 class Calculator:
+    def __init__(self, name):
+        self.name = name
+        print(name)
+        
     def add(self, num1, num2):
-        return num1 + num2
+        return num1 + num2, self.name
     
     def dog(self):
-        return "bow bow!"
+        return "bow bow!", self.name
     
-c = Calculator()
+c = Calculator("guddu")
 
 print(c.add(3,5))
+
+c2 = Calculator("duggu")
+print(c2.dog())
