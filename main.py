5c8339bc80c187cc5a0c45881e9e2ca3948ba0eb
bakul takthar <bakul takthar>
2023-02-01 21:40:07 +0530

    working with multiple classes

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
-print("hello world")
-class Calculator:
-    def __init__(self, name, age):
+class Superheroes():
+    def __init__(self, name, age, alias, powers):
         self.name = name
         self.age = age
+        self.alias = alias
+        self.powers = powers
+        self.info = [self.name, self.age, self.alias, self.powers]
         
-    def add(self, num1, num2):
-        return num1 + num2, self.name
-    
     def get_name(self):
         return self.name
     
-    def new_name(self, name):
-        self.name = name
-        
     def get_age(self):
         return self.age
     
-    def set_age(self, age):
-        self.age = age
+class Avengers():
+    def __init__(self, name, max):
+        self.name = name
+        self.max = max
+        self.heroes = []
         
+    def add_hero(self, hero):
         
-c = Calculator("guddu", 67)
+        if len(self.heroes) < self.max:
+            self.heroes.append(hero)
+            return True
+        
+        else:
+            return False
+    
+    def mean_age(self):
+        sum = 0
+        for hero in self.heroes:
+            sum += hero.get_age()
+            
+        return sum // len(self.heroes)
+    
+    
+    
+h1 = Superheroes("tony stark", 56, "iron man", "super suit")
+h2 = Superheroes("the vision", 14, "the vision", "android abilities")
+h3 = Superheroes("stephen strange", 67, "doctor strange", "wizardry")
 
-print(c.get_age())
-print(c.get_name())
+team = Avengers("avengers", 3)
+team.add_hero(h1)
+team.add_hero(h2)
+team.add_hero(h3)
 
-print(".... \n\n\n\n\n .....")
+print(team.heroes[0].info)
+print(team.heroes[1].info)
+print(team.heroes[2].info)
 
-c.set_age(69)
-c.new_name("yo mama")
-print(c.get_age())
-print(c.get_name())
+print(team.mean_age())
+
+print(team.add_hero(h1))
