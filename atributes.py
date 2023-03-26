33438e144ef4cff2753508bddad5ddbfbabc3200
bakultakthar <bakul184bsingh@gmail.com>
2023-03-26 21:58:34 +0530

    minor changes

--- atributes.py
+++ atributes.py
@@ -4,9 +4,9 @@ class Person:
     def __init__(self, name):
         self.name = name 
         Person.number_of_people += 1
-Person.number_of_people = 34        
+# Person.number_of_people = 34        
         
 p1 = Person("bakul")
 p2 = Person("aadu paadu")
 
-print(p2.number_of_people)
+print(Person.number_of_people)
